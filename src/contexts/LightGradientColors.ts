889307a91521394676a8e8bca3781e5775b936cc
chamindujayasinghe <chamindujaya12@gmail.com>
2026-05-13 18:00:14 +0530

    Resolve merge conflict: Clean up duplicate files from old app structure

--- src/contexts/LightGradientColors.ts
+++
@@ -1,6 +0,0 @@
-import { ColorValue } from "react-native";
-import colors from "../colors";
-
-const lightGradientColors: ColorValue[] = [colors.white, colors.light];
-
-export default lightGradientColors;
